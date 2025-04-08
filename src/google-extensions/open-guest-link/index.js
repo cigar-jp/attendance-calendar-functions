5b657d24ac86d250bb80f44c64cd39fd5dc8e976
cigar-jp <ryo.shiga.banana@gmail.com>
2025-04-09 06:37:04 +0900

    feat: インコグニートウィンドウで開くURLを更新

--- src/google-extensions/open-guest-link/index.js
+++ src/google-extensions/open-guest-link/index.js
@@ -2,7 +2,7 @@
 chrome.action.onClicked.addListener(() => {
   // インコグニート（シークレット）ウィンドウで指定URLを開く
   chrome.windows.create({
-    url: 'https://meetings.hubspot.com/treatment/120min',
+    url: 'https://meetings.hubspot.com/treatment/coupletreatmentmedel?uuid=47be5bff-5f1d-4aed-b026-fd27eafb185b',
     incognito: true,
   });
 });
